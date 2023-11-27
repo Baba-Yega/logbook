f7dac221a93996c25f735ff810bb7e6010aa72d0
Leonard Eguriase <leguriase98@gmail.com>
2023-11-27 12:37:38 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,30 @@
+<?php include "inc/header.php" ?>
+
+<style>
+  
+</style>
+<section>
+<div class="container">
+<div class="jumbotron bg-primary text-center">
+  <h1 class="display-4">Hello!</h1>
+  <p class="lead">Welcome to the electronic logbook for Student’s Industrial Work Experience Scheme .</p>
+  <hr class="my-1">
+  <p class="text-white">You can login as a student, administrator or an admin depending on your role in the STUDENTS INDUSTRIAL WORK EXPERIENCE SCHEME (SIWES).</p>
+  <p class="lead">
+    <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
+  </p>
+</div>
+</div>
+</section>
+
+
+
+
+
+
+
+
+
+
+
+<?php include "inc/footer.php" ?>
